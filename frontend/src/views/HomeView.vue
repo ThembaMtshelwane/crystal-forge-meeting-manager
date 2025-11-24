7bcc4297f39546a3a0a343cd22fdf5dd08604652
Themba Mtshelwane <Thembamm3@gmail.com>
2025-11-25 00:21:48 +0200

    feat: Simple homepage nav

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const display = useDisplay();
+</script>
 
 <template>
-  <h1>Home Page</h1>
+  <v-app>
+    <v-app-bar class="px-4">
+      <div class="w-full md:w-[1050px] mx-auto flex items-center">
+        <v-toolbar-title class="font-bold!">
+          {{ display.smAndUp.value ? " Crystal Forge Meets" : "CTM" }}
+        </v-toolbar-title>
+        <v-btn> Login </v-btn>
+        <v-btn> SignUp </v-btn>
+      </div>
+    </v-app-bar>
+  </v-app>
 </template>
